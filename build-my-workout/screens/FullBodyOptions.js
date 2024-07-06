4e5c812853517c7b7e35e2d295b4641a42d89d2b
Katya Greimes <katyagreimes@gmail.com>
2024-07-06 10:04:47 -0700

    FullBodyOptions Screen

---
+++ build-my-workout/screens/FullBodyOptions.js
@@ -0,0 +1,24 @@
+import { FlatList } from 'react-native';
+import { Avatar, ListItem } from 'react-native-elements';
+
+const FullBodyOptions = (props) => {
+    const renderDirectoryItem = ({ item: exercise }) => {
+        return (
+            <ListItem>
+                <Avatar source={exercise.image} rounded />
+                <ListItem.Content>
+                    <ListItem.Title>{exercise.name}</ListItem.Title>
+                </ListItem.Content>
+            </ListItem>
+        );
+    };
+    return (
+        <FlatList
+            data={props.exercises}
+            renderItem={renderDirectoryItem}
+            keyExtractor={(item) => item.id.toString()}
+        />
+    );
+};
+
+export default FullBodyOptions;
--- build-my-workout/screens/MainComponent.js
+++ build-my-workout/screens/MainComponent.js
@@ -1,2 +1,11 @@
 import { useState } from "react";
 import { EXERCISES } from "../features/exercises/exercises";
+import FullBodyOptions from "./FullBodyOptions";
+
+const Main = () => {
+    const [exercises, setExercises] = useState(EXERCISES);
+    return <FullBodyOptions exercises={exercises}/>;
+};
+
+export default Main;
+
